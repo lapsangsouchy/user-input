c590971ac4ce99796a7fc44436c367167f701d27
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2024-07-18 15:42:05 -0400

    Solutions Added

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // get references
+let textbox = document.getElementById('boxtext');
+let clickButton = document.querySelector('button');
+let screenDiv = document.querySelector('div');
+let bodyRef = document.querySelector('body');
 
 // set up onclick for clickButton
+clickButton.onclick = function (event) {
+  // prevent the page from reloading
+  event.preventDefault();
+  // capture user input
+  let userInput = textbox.value;
+  console.log(userInput);
+  // change backgroundColor based on userInput
+  bodyRef.style.backgroundColor = userInput;
 
-// prevent the page from reloading
+  // show userInput on the screen
+  screenDiv.innerHTML = userInput;
 
-// capture user input
-
-// change backgroundColor based on userInput
-
-// show userInput on the screen
-
-// Clear input
+  // Clear input
+  textbox.value = '';
+};
